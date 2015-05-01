d185bc9389327073ce34bac317b33236fbef8362
Hongyi Xin <gohongyi@gmail.com>
2015-05-01 05:50:25 -0400

    Adding optimalSolverLN

---
+++ optimalSolverLN.h
@@ -0,0 +1,62 @@
+#include "HashTree.h"
+#include <vector>
+
+using namespace std;
+
+struct Cell {
+	Cell() {
+		start = -1;
+		end = -1;
+		isleaf = false;
+		frequency = -1;
+		lstart = -1;
+		lend = -1;
+		lfreq = -1;
+		rstart = -1;
+		rend = -1;
+		rfreq = -1;
+	};
+
+	unsigned int start;
+	unsigned int end;
+	bool isleaf;
+	unsigned int frequency;
+
+	unsigned int lstart;
+	unsigned int lend;
+	unsigned int lfreq;
+	unsigned int rstart;
+	unsigned int rend;
+	unsigned int rfreq;
+};
+
+class OptimalSolver {
+public:
+	OptimalSolver();
+	~OptimalSolver();
+	void loadTree(string treeFileName);
+	void generateTree(string treeFileName);
+	void init(int readLength, int seedNum);
+	void reset();
+	unsigned solveDNA(string DNA);
+	void print();
+
+private:
+	//This is for debugging
+	void feedL0();
+	bool L0Loaded;
+	
+	HashTree tree;
+
+	int readLength;
+	int seedNum;
+
+	Cell* matrix;
+	Cell* base;
+	vector<Cell*> level;
+	vector<Cell*> level0;
+	Cell* defaultMatrix;
+	Cell* defaultbase;
+	int matrixSize;
+	int baseSize;
+};
